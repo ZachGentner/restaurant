1b6ed9d646de642fea7246983394d903a86c9c11
Zach Gentner <zach.gentner@gmail.com>
2023-09-11 17:07:22 -0400

    init

--- src/culture.js
+++ src/culture.js
@@ -6,12 +6,19 @@ function renderCulture() {
     img.setAttribute('alt', 'Los Pollos Hermanos Logo');
 
     let h1 = document.createElement('h1');
-    h1.innerText = 'Los Pollos Hermanos';
+    h1.innerText = 'Culture';
+
+    let copy = document.createElement("div");
 
     let p = document.createElement('p');
-    p.innerText = 'Culture';
+    p.innerText = 'Los Pollos Hermanos knows what family tastes like and we bring that warmth and goodness into our food every single day. We always have something delicious cooking, be it our crispy fried chicken, new spicy curly fries, or delicious dipping sauces.';
+    copy.appendChild(p);
+    
+    p = document.createElement('p');
+    p.innerText = 'We know how important it is to feed your family right, and we hold those values near and dear to our heart. Every meal we feed you will be FLAVORFUL, WARM, FRESH, and filled with LOVE and CARE. That is our promise to you.'
+    copy.appendChild(p);
 
     content.appendChild(img);
     content.appendChild(h1);
-    content.appendChild(p);
+    content.appendChild(copy);
 }
--- src/jobs.js
+++ src/jobs.js
@@ -6,12 +6,31 @@ function renderJobs() {
     img.setAttribute('alt', 'Los Pollos Hermanos Logo');
 
     let h1 = document.createElement('h1');
-    h1.innerText = 'Los Pollos Hermanos';
+    h1.innerText = 'Jobs';
+
+    let copy = document.createElement("div");
+
+    let h2 = document.createElement('h2');
+    h2.innerText = "Restaurant Manager";
+    copy.appendChild(h2);
 
     let p = document.createElement('p');
-    p.innerText = 'Jobs';
+    p.innerText = 'Job Responsibilities and Duties';
+    copy.appendChild(p);
+
+    p = document.createElement('p');
+    p.innerText = 'Manage, train and discipline employees';
+    copy.appendChild(p);
+    
+    p = document.createElement('p');
+    p.innerText = 'Order supplies as needed'
+    copy.appendChild(p);
+
+    p = document.createElement('p');
+    p.innerText = 'Interview candidates for potential employment.'
+    copy.appendChild(p);
 
     content.appendChild(img);
     content.appendChild(h1);
-    content.appendChild(p);
+    content.appendChild(copy);
 }
